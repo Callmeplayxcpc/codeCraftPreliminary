20bfe46cfa08f9edd8ed9e3ca8ecb25bae6e4eaa
scutsky <1938629638@qq.com>
2025-03-26 17:05:32 +0800

    add example_extra to go.sh  &&  optimize CMake

--- go.sh
+++ go.sh
@@ -11,3 +11,4 @@ cd ..
 # run test case
 python3 run.py ./interactor ./data/sample.in ./code_craft
 python3 run.py ./interactor ./data/sample_practice.in ./code_craft
+python3 run.py ./interactor ./data/sample_extra.in ./code_craft
