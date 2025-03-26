3ae3467638312586e2e31782b7995cbafc94eebd
徐睿 <2300038520@qq.com>
2025-03-26 17:27:57 +0800

    35

--- main.cpp
+++ main.cpp
@@ -16,22 +16,6 @@ int main()
         G:每个磁头每个时间片最多消耗的令牌数 64<=G<=1000
     */
 
-<<<<<<< HEAD
-    // 全局预处理阶段：读取三个分块数据（删除、写入、读取的统计数据，此处用 %*d 跳过）
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
-            scanf("%d",&fre_del[i][j]);
-        }
-    }
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
-            scanf("%d",&fre_write[i][j]);
-        }
-    }
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
-            scanf("%d",&fre_read[i][j]);
-=======
     // 全局预处理阶段：读取三个分块数据（删除、写入、读取的统计数据）
     for (int i = 1; i <= M; i++)
     {
@@ -55,7 +39,6 @@ int main()
             scanf("%d", &fre_read[i][j]);
             tag_read[i]+=fre_read[i][j];
             total_read+=fre_read[i][j];
->>>>>>> 20bfe46cfa08f9edd8ed9e3ca8ecb25bae6e4eaa
         }
     }
 
@@ -69,11 +52,6 @@ int main()
     printf("OK\n");
     fflush(stdout);
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 20bfe46cfa08f9edd8ed9e3ca8ecb25bae6e4eaa
     // 按时间片循环处理各类交互事件
     for (int t = 1; t <= T + EXTRA_TIME; t++)
     {
